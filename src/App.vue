4249d720861f1da296766955608e3d81edeb141f
Punyawee Sornpanya <punyawee.sor@student.mahidol.ac.th>
2025-09-11 12:53:36 +0700

    update LoginModel

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script setup>
+import { ref } from 'vue'
+
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
+import LoginModal from './components/LoginModal.vue'
+
+const showLogin = ref(false)
+function openLogin() { showLogin.value = true }
+function handleLogin(payload) {
+  // TODO: call API login
+  console.log('login:', payload)
+  showLogin.value = false
+}
 </script>
 
 <template>
   <div>
-    <NavBar />
+      <NavBar @open-login="openLogin" />
+
     <router-view />
     <Footer />
+    <LoginModal v-model="showLogin" @login="handleLogin" />
   </div>
 </template>
