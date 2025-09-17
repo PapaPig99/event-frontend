0ce67a75877145776b58411ea186de28118dd022
thitiP <thitiporn.pah@student.mahidol.ac.th>
2025-09-17 13:29:09 +0700

    fix router + App.vue + main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index.js'   // ✅ default import
+import router  from './router'
+import './style.css'
 createApp(App).use(router).mount('#app')
