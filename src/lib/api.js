8d51beb41eb8875365fdbc8e7544f5fc3acad6d6
puodwanli <wanlida.sup@student.mahidol.edu>
2025-09-24 22:50:56 +0700

    Edit Overview

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import axios from 'axios'
 
 const api = axios.create({
@@ -11,3 +12,10 @@ api.interceptors.request.use(cfg => {
 })
 
 export default api
+=======
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:3137",
+});
+>>>>>>> Stashed changes
