a6499bde64b0346e625f73fdafc22f8b5b301096
FernPonpatPunthong <pornpat.pun@student.mahidol.edu>
2025-09-25 23:56:45 +0700

    chore: แก้ไฟล์ให้ตรงกับ main

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import axios from 'axios'
 
 const api = axios.create({
@@ -12,10 +11,3 @@ api.interceptors.request.use(cfg => {
 })
 
 export default api
-=======
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:3137",
-});
->>>>>>> Stashed changes
